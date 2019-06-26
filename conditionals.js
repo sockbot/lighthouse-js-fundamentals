197042708c5669206bb99d2fa56f063616561998
sockbot <github@sockbot.com>
2019-06-26 18:28:41 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -8,5 +8,12 @@ if (temperature < 0) {
   console.log("Short sleeves are fine.");
 }
 
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
 console.log("Now you're ready to go outside!");
 
